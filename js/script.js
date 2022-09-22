c92b51451c5ba514a51472b265bd298fd022873b
Kekandopala <98645374+Kekandopala@users.noreply.github.com>
2022-09-22 17:55:50 +0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,29 @@
+const swiper = new Swiper('.partners-slider', {
+   speed: 400,
+   spaceBetween: 100,
+   navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    },
+    slidesPerView: 1,
+ });
+
+  // Меню бургер
+$(document).ready(function(){
+   $('.menu__icon').click(function(event){
+      $('.menu__icon,.menu__body').toggleClass('_active');
+      $('body').toggleClass('_lock');
+      $('.overlay').toggleClass('overlay-active');
+   });
+});
+
+$(document).ready(function(){
+   $('.menu__body._active,.menu__link').click(function(event){
+      $('.menu__icon,.menu__body').removeClass('_active');
+      $('body').removeClass('_lock');
+      $('.overlay').removeClass('overlay-active');
+   });
+});
+
+
+
